4b1e78fd9a8b73ae896450df8fc7eb39d316a4f9
idli69 <idlidev69@gmail.com>
2026-07-07 11:26:51 +0530

    Initial scaffold: Svelte 5 + Tailwind v4 + Vite 8

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+    let count = $state(0);
+</script>
+
+<main class="flex flex-col items-center justify-center min-h-screen gap-2">
+    <h1 class="text-5xl">Hello, World!</h1>
+
+    <span class="mt-5 text-xl text-mist">Count: {count}</span>
+    <button class="button" onclick={() => count++}> Increment </button>
+</main>
+
+<style></style>
