4147f0b71b2e40351a49f3ff522134c8c91069bc
idli69 <idlidev69@gmail.com>
2026-08-11 22:15:46 +0530

    restart building the whole template

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,89 @@
 <script>
-    let count = $state(0);
-    const increment = () => count++;
-    const decrement = () => (count > 0 ? count-- : (count = 0));
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from './assets/vite.svg'
+  import heroImg from './assets/hero.png'
+  import Counter from './lib/Counter.svelte'
 </script>
 
-<div class="hero bg-base-300 min-h-screen">
-    <div class="hero-content text-center">
-        <div class="max-w-md">
-            <p class="py-5 text-4xl">
-                Count = {count}
-            </p>
-            <button class="btn btn-accent btn-soft mx-2" onclick={decrement}>
-                -
-            </button>
-            <button class="btn btn-primary btn-soft mx-2" onclick={increment}>
-                +
-            </button>
-        </div>
-    </div>
-</div>
+<section id="center">
+  <div class="hero">
+    <img src={heroImg} class="base" width="170" height="179" alt="" />
+    <img src={svelteLogo} class="framework" alt="Svelte logo" />
+    <img src={viteLogo} class="vite" alt="Vite logo" />
+  </div>
+  <div>
+    <h1>Get started</h1>
+    <p>Edit <code>src/App.svelte</code> and save to test <code>HMR</code></p>
+  </div>
+  <Counter />
+</section>
+
+<div class="ticks"></div>
+
+<section id="next-steps">
+  <div id="docs">
+    <svg class="icon" role="presentation" aria-hidden="true">
+      <use href="/icons.svg#documentation-icon"></use>
+    </svg>
+    <h2>Documentation</h2>
+    <p>Your questions, answered</p>
+    <ul>
+      <li>
+        <a href="https://vite.dev/" target="_blank" rel="noreferrer">
+          <img class="logo" src={viteLogo} alt="" />
+          Explore Vite
+        </a>
+      </li>
+      <li>
+        <a href="https://svelte.dev/" target="_blank" rel="noreferrer">
+          <img class="button-icon" src={svelteLogo} alt="" />
+          Learn more
+        </a>
+      </li>
+    </ul>
+  </div>
+  <div id="social">
+    <svg class="icon" role="presentation" aria-hidden="true">
+      <use href="/icons.svg#social-icon"></use>
+    </svg>
+    <h2>Connect with us</h2>
+    <p>Join the Vite community</p>
+    <ul>
+      <li>
+        <a href="https://github.com/vitejs/vite" target="_blank" rel="noreferrer">
+          <svg class="button-icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#github-icon"></use>
+          </svg>
+          GitHub
+        </a>
+      </li>
+      <li>
+        <a href="https://chat.vite.dev/" target="_blank" rel="noreferrer">
+          <svg class="button-icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#discord-icon"></use>
+          </svg>
+          Discord
+        </a>
+      </li>
+      <li>
+        <a href="https://x.com/vite_js" target="_blank" rel="noreferrer">
+          <svg class="button-icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#x-icon"></use>
+          </svg>
+          X.com
+        </a>
+      </li>
+      <li>
+        <a href="https://bsky.app/profile/vite.dev" target="_blank" rel="noreferrer">
+          <svg class="button-icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#bluesky-icon"></use>
+          </svg>
+          Bluesky
+        </a>
+      </li>
+    </ul>
+  </div>
+</section>
+
+<div class="ticks"></div>
+<section id="spacer"></section>
